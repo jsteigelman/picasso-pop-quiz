98b9627912a1fe4f8e4b5630ac99070ec222c5ff
Joey Steigelman <joey.steigelman@gmail.com>
2021-12-09 16:41:26 -0500

    set up QuestionSO

---
+++ Assets/Scripts/QuestionSO.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Question", menuName = "Quiz Question", order = 0)]
+public class QuestionSO : ScriptableObject {
+
+    // question box in inspector panel
+    [TextArea(2,6)]
+    [SerializeField] string question = "New question text goes here";
+    [SerializeField] string[] answers = new string[4];
+    [SerializeField] int correctAnswerIndex;
+
+    public string GetQuestion() {
+        return question;
+    }
+
+    public string GetAnswer (int index) {
+        return answers[index];
+    }
+
+    public int GetCorrectAnswerIndex() {
+        return correctAnswerIndex;
+    }
+}
