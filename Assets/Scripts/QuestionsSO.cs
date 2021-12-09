42ab2341e1bcfb69ab9de7ef0b5ef0c7ac8d487e
Joey Steigelman <joey.steigelman@gmail.com>
2021-12-09 16:19:20 -0500

    set up answers array

--- Assets/Scripts/QuestionsSO.cs
+++ Assets/Scripts/QuestionsSO.cs
@@ -8,8 +8,18 @@ public class QuestionsSO : ScriptableObject {
     // question box in inspector panel
     [TextArea(2,6)]
     [SerializeField] string question = "New question text goes here";
+    [SerializeField] string[] answers = new string[4];
+    [SerializeField] int correctAnswerIndex;
 
     public string GetQuestion() {
         return question;
+    }
+
+    public string GetAnswer (int index) {
+        return answers[index];
+    }
+
+    public int GetCorrectAnswerIndex() {
+        return correctAnswerIndex;
     }
 }
